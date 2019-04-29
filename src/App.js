0e64e630411407e21f35d3c98f6e5460b178b5c3
Miloud Lafttah <miloud.lafttah@tersea.com>
2019-04-29 18:42:26 +0100

    list data + style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import axios from "axios";
+import moment from "moment";
 
 const URL =
   "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc";
@@ -11,9 +12,33 @@ const App = () => {
     axios.get(URL).then(res => setData(res.data.items));
   }, []);
 
-  console.log(data);
-
-  return <div>Hello World!!!</div>;
+  return (
+    <Fragment>
+      {data.map(item => {
+        return (
+          <div key={item.id} className="card">
+            <div className="avatar">
+              <img src={item.owner.avatar_url} alt="user avatar" />
+              <div>{item.owner.login}</div>
+            </div>
+            <div className="detail">
+              <div className="name">{item.name}</div>
+              <div className="desc">{item.description}</div>
+              <div className="marks">
+                <span className="stars">Stars: {item.stargazers_count}</span>
+                <span className="issues">Issues: {item.open_issues_count}</span>
+                <span className="created">
+                  {`Submitted ${moment(item.created_at).fromNow()} by ${
+                    item.owner.login
+                  }`}
+                </span>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </Fragment>
+  );
 };
 
 export default App;
