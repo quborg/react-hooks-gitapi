24d36ce4b2674ce03569d80a65b013b4fe843b07
Miloud Lafttah <miloud.lafttah@tersea.com>
2019-04-29 18:34:21 +0100

    get remote data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const App = () => <div>Hello World !!!</div>;
+const URL =
+  "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc";
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get(URL).then(res => setData(res.data.items));
+  }, []);
+
+  console.log(data);
+
+  return <div>Hello World!!!</div>;
+};
 
 export default App;
